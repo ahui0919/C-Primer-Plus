e6add72dadbe279360b448fbd6688b718d83749e
Joker hui <ahui@rockylinux.com>
2025-04-08 19:27:08 +0800

    Modify: modified print1.c

--- ch3/print1.c
+++ ch3/print1.c
@@ -1,12 +1,16 @@
 /* print1.c-displays some properties of printf() */
 #include <stdio.h>
-int main(void)
+#include<stdlib.h>
+
+int main(int argc, char** argv)
 {
     int ten = 10;
     int two = 2;
+
     printf("Doing it right: ");
     printf("%d minus %d is %d\n", ten, 2, ten - two);
     printf("Doing it wrong: ");
     printf("%d minus %d is %d\n", ten); // forgot 2 arguments
-    return 0;
+
+    return EXIT_SUCCESS;
 }
