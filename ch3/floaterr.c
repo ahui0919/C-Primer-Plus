acc905c93b88a29292459925ce92a83ce21a76af
Joker hui <ahui@rockylinux.com>
2025-04-10 11:44:45 +0800

    MODIFY: modified floaterr.c

--- ch3/floaterr.c
+++ ch3/floaterr.c
@@ -1,10 +1,14 @@
 /* floaterr.c--demonstrates round-off error */
 #include <stdio.h>
-int main(void)
+#include <stdlib.h>
+
+int main(int arc, char **argv)
 {
     float a, b;
     b = 2.0e20 + 1.0;
     a = b - 2.0e20;
+
     printf("%f \n", a);
-    return 0;
+    
+    return EXIT_SUCCESS;
 }
