884b4bcf64acff77729edd5d3819907ad837f61f
Joker hui <ahui@rockylinux.com>
2025-04-10 16:33:49 +0800

    MODIFY: format typesize.c

--- ch3/typesize.c
+++ ch3/typesize.c
@@ -4,16 +4,16 @@
 
 int main(int argc, char **argv)
 {
-    /* c99 provides a %zd specifier for sizes */
-    printf("Type int has a size of %zd bytes.\n", sizeof(int));
-    printf("Type char has a size of %zd bytes.\n", sizeof(char));
-    printf("Type long has a size of %zd bytes.\n", sizeof(long));
-    printf("Type long long has a size of %zd bytes.\n",
-           sizeof(long long));
-    printf("Type double has a size of %zd bytes.\n",
-           sizeof(double));
-    printf("Type long double has a size of %zd bytes.\n",
-           sizeof(long double));
-           
-    return EXIT_SUCCESS;
+       /* c99 provides a %zd specifier for sizes */
+       printf("Type int has a size of %zd bytes.\n", sizeof(int));
+       printf("Type char has a size of %zd bytes.\n", sizeof(char));
+       printf("Type long has a size of %zd bytes.\n", sizeof(long));
+       printf("Type long long has a size of %zd bytes.\n",
+              sizeof(long long));
+       printf("Type double has a size of %zd bytes.\n",
+              sizeof(double));
+       printf("Type long double has a size of %zd bytes.\n",
+              sizeof(long double));
+
+       return EXIT_SUCCESS;
 }
