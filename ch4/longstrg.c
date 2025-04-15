b95339442d5ad1c4e8fed5f321efd107d575de80
Joker hui <ahui@rockylinux.com>
2025-04-15 23:08:44 +0800

    ADD: add longstrg.c

---
+++ ch4/longstrg.c
@@ -0,0 +1,15 @@
+/* longstrg.c -- printing long strings */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    printf("Here's one way to print a ");
+    printf("long string.\n");
+    printf("Here's another way to print a \
+long string.\n");
+    printf("Here's the newest way to print a "
+    "long string.\n");
+
+    return EXIT_SUCCESS;
+}
