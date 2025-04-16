c84ab49495dd4531f62b1a3bf8a1683f1268252d
Joker hui <ahui@rockylinux.com>
2025-04-16 17:24:30 +0800

    ADD: add shoes2.c -- calculates foot lengths for several sizes

---
+++ ch5/shoes2.c
@@ -0,0 +1,29 @@
+/* shoes2.c -- calculates foot lengths for several sizes */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ADJUST 7.31
+
+int main(int argc, char **argv)
+{
+    const double SCALE = 0.333;
+    double shoe;
+    double foot;
+
+    printf("Shoe size (men's)     foot length\n");
+    shoe = 3.0;
+    
+    /* Starting the while loop */
+    while (shoe < 18.5)
+    {
+        /* start of block */
+        foot = SCALE * shoe + ADJUST;
+        printf("%10.1f %15.2f inches\n", shoe, foot);
+        shoe = shoe + 1.0;
+        /* end of block */
+    }       
+    
+    printf("If the shoe fits, wear it.\n");
+    
+    return EXIT_SUCCESS;
+}
