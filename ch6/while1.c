94b2bc2c6009460734e6c036a0bb2264104fd437
Joker hui <ahui@rockylinux.com>
2025-04-20 23:52:49 +0800

    ADD: add while1.c -- watch  your brances

---
+++ ch6/while1.c
@@ -0,0 +1,18 @@
+/* while1.c -- watch  your brances */
+/* bad coding creates an infinite loop*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    int n = 0;
+
+    while(n < 3) {
+        printf("n is %d\n", n);
+        n++;
+    }
+
+    printf("That's all this program does\n");
+    
+    return EXIT_SUCCESS;
+}
