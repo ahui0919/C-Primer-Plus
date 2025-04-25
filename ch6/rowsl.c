0a0367aa2e9e160133e7459164479e4849ef157a
Joker hui <ahui@rockylinux.com>
2025-04-25 14:46:08 +0800

    ADD: add rowsl.c -- uses nested loops

---
+++ ch6/rowsl.c
@@ -0,0 +1,21 @@
+/* rowsl.c -- uses nested loops */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 6
+#define CHARS 10
+
+int main(int argc, char **argv)
+{
+    int row;
+    char ch;
+
+    for (row = 0; row < ROWS; row++) {
+        for (ch = 'A'; ch < ('A' + CHARS); ch++) {
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+    
+    return EXIT_SUCCESS;
+}
