b0d9787d9be1aa4617f37c98df70d658b56ae294
lenovo_ahui <ahui@Virtual_lenovo.com>
2025-04-29 11:02:47 +0800

    MODIFY: modified vowels.c

--- ch7/vowels.c
+++ ch7/vowels.c
@@ -5,46 +5,45 @@
 int main(int argc, char **argv)
 {
     char ch;
-    int a_ct;
-    int e_ct;
-    int i_ct;
-    int o_ct;
-    int u_ct;
-
-    a_ct = 0;
-    e_ct = 0;
-    i_ct = 0;
-    o_ct = 0;
-    u_ct = 0;
+    int a_ct = 0;
+    int e_ct = 0;
+    int i_ct = 0;
+    int o_ct = 0;
+    int u_ct = 0;
 
     printf("Enter some text; enter # to quit.\n");
 
     while ((ch = getchar()) != '#') {
         switch (ch) {
         case 'a':
-        case 'A':
+        case 'A': {
             a_ct++;
             break;
+        }
         
         case 'e':
-        case 'E':
+        case 'E': {
             e_ct++;
             break;
+        }
 
         case 'i':
-        case 'I':
+        case 'I': {
             i_ct++;
             break;
+        }
 
         case 'o':
-        case 'O':
+        case 'O': {
             o_ct++;
             break;
+        }
 
         case 'u':
-        case 'U':
+        case 'U': {
             u_ct++;
             break;
+        }
     
         default:
             break;
